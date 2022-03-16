4dadbea569efd356390af403dd1aee9dd5202ecd
lukaszgaza <g.lukasz@gmail.com>
2022-03-16 22:43:22 +0100

    Finished Singleton exercise

--- src/test/java/SingletonTest.java
+++ src/test/java/SingletonTest.java
@@ -1,5 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SingletonTest {
+
+    @Test
+    public void shouldReturnSingletonInstance() {
+        assertNotNull(Singleton.getInstance());
+    }
+
+    @Test
+    public void shouldNotHavePublicConstructor() {
+        Class<Singleton> clazz = Singleton.class;
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> constructor: constructors) {
+            if (Modifier.isPublic(constructor.getModifiers()) || Modifier.isProtected(constructor.getModifiers())) {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    public void shouldAlwaysReturnTheSameInstance() {
+        assertTrue(Singleton.getInstance() == Singleton.getInstance());
+    }
 
 }
